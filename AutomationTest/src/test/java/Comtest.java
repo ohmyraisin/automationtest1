36f295377033c9ad2b84d8270a47f28279a64a69
jeong-eunju <jeong-eunju@218.38.137.27>
2020-06-27 19:20:52 +0900

    test

---
+++ AutomationTest/src/test/java/Comtest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+public class Comtest {
+	
+	@Test
+	public void s01 ( )
+	{
+		System.out.println("test");
+	}
+	
+//	@BeforeTest
+//	public void StartBrowser_NavURL() {
+//	       driver = new ChromeDriver();
+//	       driver.manage().window().maximize();
+//	}
+//
+//	@AfterTest
+//	public void CloseBrowser() {
+//	       driver.quit();
+//	}
+//
+//	@Test
+//	public void testToCompareDoubles() {
+//	       driver.get("http://www.google.com");
+//	}
+}
